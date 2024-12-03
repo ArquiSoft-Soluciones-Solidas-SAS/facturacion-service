fa67d39107e2fbe1aa199058402de03c3a1ca23f
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-02 19:02:45 -0500

    urls

--- ofipensiones/facturacionService/urls.py
+++ ofipensiones/facturacionService/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listar-recibos-cobro/', views.getRecibosCobro, name='listar_recibos_cobro'),
-    path('listar-recibos-pago/', views.getRecibosPago, name='listar_recibos_pago'),
+    path('facturacion/listar-recibos-cobro/', views.getRecibosCobro, name='listar_recibos_cobro'),
+    path('facturacion/listar-recibos-pago/', views.getRecibosPago, name='listar_recibos_pago'),
     ]
