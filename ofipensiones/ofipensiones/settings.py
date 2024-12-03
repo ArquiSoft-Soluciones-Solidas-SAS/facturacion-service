50fc904924987b940017f3e82224cf7184324cbc
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-02 21:27:15 -0500

    url gateway

--- ofipensiones/ofipensiones/settings.py
+++ ofipensiones/ofipensiones/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,7 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+PATH_API_GATEWAY = "http://" + os.environ.get("KONG_HOST", "10.128.0.81") + ":" + os.environ.get("KONG_PORT", "8000")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
